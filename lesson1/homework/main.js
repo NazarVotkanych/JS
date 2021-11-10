d1b2758e655909f02e5c3159a581ae72b047c22a
NazarVotkanych <nazarvotkanych@gmail.com>
2021-11-11 01:04:09 +0200

    New step

--- lesson1/homework/main.js
+++ lesson1/homework/main.js
@@ -24,68 +24,203 @@
 // alert(d);
 // document.write(d);
 //
-// let num1 = '1';
-//
-// console.log(num1);
-// alert(num1);
-// document.write(num1);
+//  let num1 = 1;
+//  console.log(num1);
+//  alert(num1);
+//  document.write(num1);
 
-// let num2 = '100';
+// let num2 = 100;
 //
 // console.log(num2);
 // alert(num2);
 // document.write(num2);
 //
-// let num3 = '999';
+// let num3 = 999;
 //
 // console.log(num3);
 // alert(num3);
 // document.write(num3);
 //
-// let num4 = '123';
+// let num4 = 123;
 //
 // console.log(num4);
 // alert(num4);
 // document.write(num4);
 //
-// let num5 = '3.14';
+// let num5 = 3.14;
 //
 // console.log(num5);
 // alert(num5);
 // document.write(num5);
 //
 //
-// let num6 = '2.44';
+// let num6 = 2.44;
 //
 // console.log(num6);
 // alert(num6);
 // document.write(num6);
 //
-// let num7 = '16';
+// let num7 = 16;
 //
 // console.log(num7);
 // alert(num7);
 // document.write(num7);
 
-// let t = 'true'
+// let t = true
 //
 // console.log(t);
 // alert(t);
 // document.write(t);
 //
-// let f = 'false'
+// let f = false
 //
 // console.log(f);
 // alert(f);
 // document.write(f);
-0
 
 
 // - Переприсвоїти кожній змінній з завдання значення на довільне.
 //     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+// let a = 'hello';
+// console.log(a);
+// alert(a);
+// document.write(a);
+// a = 'Hello World'
+// console.log(a);
+// alert(a);
+// document.write(a);
+//
+// let b = 'owu';
+// console.log(b);
+// alert(b);
+// document.write(b);
+//
+// b = 'okten'
+// console.log(b);
+// alert(b);
+// document.write(b);
+//
+// let c = 'com';
+// console.log(c);
+// alert(c);
+// document.write(c);
+// c = 'com.ua'
+// console.log(c);
+// alert(c);
+// document.write(c);
+//
+// let d = 'ua';
+// console.log(d);
+// alert(d);
+// document.write(d);
+//
+// d = 'uaua'
+// console.log(d);
+// alert(d);
+// document.write(d);
+//
+//  let num1 = 1;
+// console.log(num1);
+// alert(num1);
+// document.write(num1);
+//  num1 = 11
+//  console.log(num1);
+//  alert(num1);
+//  document.write(num1);
+//
+// let num2 = 100;
+// console.log(num2);
+// alert(num2);
+// document.write(num2);
+// num2 = 200
+// console.log(num2);
+// alert(num2);
+// document.write(num2);
+//
+// let num3 = 999;
+// console.log(num3);
+// alert(num3);
+// document.write(num3);
+// num3 = 111
+// console.log(num3);
+// alert(num3);
+// document.write(num3);
+//
+// let num4 = 123;
+// console.log(num4);
+// alert(num4);
+// document.write(num4);
+// num4 = 321
+// console.log(num4);
+// alert(num4);
+// document.write(num4);
+//
+// let num5 = 3.14;
+// console.log(num5);
+// alert(num5);
+// document.write(num5);
+// num5 = 4.13
+// console.log(num5);
+// alert(num5);
+// document.write(num5);
+//
+//
+// let num6 = 2.44;
+// console.log(num6);
+// alert(num6);
+// document.write(num6);
+// num6 = 4.22
+// console.log(num6);
+// alert(num6);
+// document.write(num6);
+//
+// let num7 = 16;
+// console.log(num7);
+// alert(num7);
+// document.write(num7);
+//
+// num7 = 61;
+// console.log(num7);
+// alert(num7);
+// document.write(num7);
+//
+// let t = true;
+// console.log(t);
+// alert(t);
+// document.write(t);
+// t = false
+// console.log(t);
+// alert(t);
+// document.write(t);
+//
+// let f = false;
+// console.log(f);
+// alert(f);
+// document.write(f);
+//
+// f = true
+// console.log(f);
+// alert(f);
+// document.write(f);
+
 // - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+// let person = [
+//     {
+//         firstName: 'Nazar',
+//         middleName: 'Juniour',
+//         LastName: 'Votkanych',
+//     }
+// ];
+// console.log(person);
+
 // - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
 //     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+let name = prompt('What is your name?');
+let surname = prompt('what is your surname?');
+let age = prompt('What is your age?');
+console.log(`Вітаю ${name} ${surname}. Тобі ${age} років.`);
+
 // - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
 //     let a = 100; let b = '100'; let c = true;
 // - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
