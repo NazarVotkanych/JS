a9a4a1aca885047c00bd87574b3b5d141f36dc8d
NazarVotkanych <nazarvotkanych@gmail.com>
2021-11-10 21:22:13 +0200

    New step

---
+++ lesson1/homework/main.js
@@ -0,0 +1,110 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// let a = 'hello';
+//
+// console.log(a);
+// alert(a);
+// document.write(a);
+//
+// let b = 'owu';
+//
+// console.log(b);
+// alert(b);
+// document.write(b);
+//
+// let c = 'com';
+//
+// console.log(c);
+// alert(c);
+// document.write(c);
+//
+// let d = 'ua';
+//
+// console.log(d);
+// alert(d);
+// document.write(d);
+//
+// let num1 = '1';
+//
+// console.log(num1);
+// alert(num1);
+// document.write(num1);
+
+// let num2 = '100';
+//
+// console.log(num2);
+// alert(num2);
+// document.write(num2);
+//
+// let num3 = '999';
+//
+// console.log(num3);
+// alert(num3);
+// document.write(num3);
+//
+// let num4 = '123';
+//
+// console.log(num4);
+// alert(num4);
+// document.write(num4);
+//
+// let num5 = '3.14';
+//
+// console.log(num5);
+// alert(num5);
+// document.write(num5);
+//
+//
+// let num6 = '2.44';
+//
+// console.log(num6);
+// alert(num6);
+// document.write(num6);
+//
+// let num7 = '16';
+//
+// console.log(num7);
+// alert(num7);
+// document.write(num7);
+
+// let t = 'true'
+//
+// console.log(t);
+// alert(t);
+// document.write(t);
+//
+// let f = 'false'
+//
+// console.log(f);
+// alert(f);
+// document.write(f);
+
+
+// - Переприсвоїти кожній змінній з завдання значення на довільне.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками.
+//     Вивести "Вітаю *Імя* *По батькові*. Тобі *вік* років".
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+// - Визначити відповідний оператор в виразах що б вийшов відповідний результат.
+//     В однакових виразах не використовувати однакові оператори!!!
+//     5 ? 6 -> true
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 5 ? 6 -> false
+// 10 ? 10 -> true
+// 10 ? 10 -> true
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 10 ? 10 -> false
+// 123 ? '123' -> false
+// 123 ? '123' -> true
+//
+// - Подумайте Какие значения выведет в окно браузера следующий фрагмент кода?  и почему?
+//     let str = "20";
+// let a = 5;
+// document.write(str + a + "<br/>");
+// document.write(str - a + "<br/>");
+// document.write(str * "2" + "<br/>");
+// document.write(str / 2 + "<br/>");
