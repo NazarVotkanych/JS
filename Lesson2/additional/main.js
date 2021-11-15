f574bb337405cf5f9fb77150ce08489b05fff2c4
NazarVotkanych <nazarvotkanych@gmail.com>
2021-11-15 18:51:51 +0200

    New step

--- Lesson2/additional/main.js
+++ Lesson2/additional/main.js
@@ -14,6 +14,13 @@
 // let result = num[Math.round((num.length - 1)/2)];
 // console.log(result)
 
+
+// let num = [5,40,6];
+// let result =  (num) => num.reduce((a, b) => a + b) / num.length;
+// console.log(result([5,40,6]));
+
+// const average = (array) => array.reduce((a, b) => a + b) / array.length;
+// console.log(average([1,2,3,4,5]));
 //
 //
 // - Перепишіть конструкцію if з використанням умовного оператора '?':
