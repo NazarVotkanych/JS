719a6e5cd350e11b72f8a3e1b79c2a1a3057ccca
NazarVotkanych <nazarvotkanych@gmail.com>
2021-12-14 00:14:49 +0200

    New step

--- Lesson12/additional/main.js
+++ Lesson12/additional/main.js
@@ -12,6 +12,7 @@ fetch(`https://jsonplaceholder.typicode.com/users`)
         let wrap = document.createElement('div')
         wrap.classList.add('wrap')
         for (const user of users) {
+         let divUsers = document.createElement('div')
             let div = document.createElement('div')
             let button = document.createElement('button')
             div.classList.add('comment')
@@ -40,7 +41,7 @@ fetch(`https://jsonplaceholder.typicode.com/users`)
 
 
             button.addEventListener('click', () => {
-                fetch(`https://jsonplaceholder.typicode.com/posts/${user.id}/posts`)
+                fetch(`https://jsonplaceholder.typicode.com/users/${user.id}/posts`)
                     .then(response => response.json())
                     .then(posts => {
                         for (const post of posts) {
@@ -53,40 +54,44 @@ fetch(`https://jsonplaceholder.typicode.com/users`)
                                     <h2>${post.id}</h2>
                                     <h3>${post.title}</h3>
                                     <p><b>Body</b>${post.body}</p>`
-                                    ;
+                                ;
 
 
                                 div.appendChild(divPosts)
-                                button1.innerText = "CLick"
+                                div.appendChild(button1)
+                                divPosts.appendChild(button1)
+                                button1.innerText = "Click Here"
                                 button1.classList.add('button1')
-
                                 button1.addEventListener('click', () => {
-                                    fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/posts`)
+                                    fetch(`https://jsonplaceholder.typicode.com/posts/${post.id}/comments`)
                                         .then(response => response.json())
                                         .then(comments => {
                                             for (const comment of comments) {
-                                                if (post.id === comment.userId){
+                                                if (post.id === comment.userId) {
                                                     let divComments = document.createElement('div')
                                                     divComments.innerHTML = `
-                                    <h2> ID: ${comment.id}</h2>
-                                            <h3>${comment.name}</h3>
-                                             <p><b>Email</b>${comment.email}</p>
-                                             <p><b>Body</b>${comment.body}</p>
-                                    `
-
+                                                <h2> ID: ${comment.id}</h2>
+                                                <h3>${comment.name}</h3>
+                                                <p><b>Email</b>${comment.email}</p>
+                                                <p><b>Body</b>${comment.body}</p>
+                                                    `
                                                     divPosts.appendChild(divComments)
-
                                                 }
                                             }
                                         });
-                                })
+                                });
+                                div.appendChild(divPosts)
+                                div.appendChild(button1)
+                                divPosts.appendChild(button1)
                             }
                         }
                     });
             })
-
-            wrap.appendChild(div)
+            divUsers.appendChild(div)
+            wrap.appendChild(divUsers)
             div.appendChild(button)
             document.body.appendChild(wrap)
         }
     });
+
+
