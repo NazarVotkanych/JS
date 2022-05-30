e09a5e9809f6bc893d1915fb09276489d17a8511
NazarVotkanych <nazarvotkanych@gmail.com>
2022-05-30 21:18:51 +0300

    11 Домашка

--- Lesson11_Again/main.js
+++ Lesson11_Again/main.js
@@ -1,29 +1,28 @@
 // -створити форму з інпутами для name та age.
 //     При відправці форми записати об'єкт в localstorage
 //
-// let name = document.getElementById('name')
-// let age = document.getElementById('age')
-// let button = document.getElementById('button')
-//
+// let name = document.getElementById("name");
+// let age = document.getElementById("age");
+// let button = document.getElementById("button");
 //
 // let send = (userName, userAge) => {
-//     let user ={
+//     let user = {
 //         name: userName,
 //         age: userAge
 //     }
-//     localStorage.setItem('userInfo',JSON.stringify(user))
+//     localStorage.setItem('UserInfo', JSON.stringify(user));
 // };
 // button.onclick = () => {
 //     send(name.value, age.value)
 // };
-
-
 // -створити форму з інпутами для model,type та volume автівки.
 //     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
 
-let model = document.getElementById('model')
-let type = document.getElementById('type')
-let volume = document.getElementById('volume')
+
+let model = document.getElementById("model");
+let type = document.getElementById("type");
+let volume = document.getElementById("volume");
+let button = document.getElementById("button")
 
 let send = (userModel, userType, userVolume) => {
     let user = {
@@ -31,17 +30,15 @@ let send = (userModel, userType, userVolume) => {
         type: userType,
         volume: userVolume
     }
-
-    let data = localStorage.getItem('CARS');
-    if (data) {
-        let cars = JSON.parse(data);
-        localStorage.setItem('CARS',JSON.stringify([...cars, user]))
+    let info = localStorage.getItem("CARS");
+    if (info) {
+        let infoCars = JSON.parse(info)
+        localStorage.setItem("CARS", JSON.stringify([...infoCars, user]))
         return;
     }
-
-    localStorage.setItem('CARS', JSON.stringify([user]))
+    localStorage.setItem("CARS", JSON.stringify([user]))
 };
+    button.onclick = () => {
+        send(model.value, type.value, volume.value)
+    };
 
-button.onclick = () => {
-    send(model.value, type.value, volume.value)
-}
